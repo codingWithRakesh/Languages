fb526e6c44a02f4a6c975b04b4e51b86c8a60b8a
skrijwan <skrijwan329@gmail.com>
2025-07-24 09:59:07 +0530

    add Solidity and language that help to write Smartcontract.

---
+++ Languages/hello.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+
+contract helloWorld{
+
+    function Show() public pure  returns(string  memory ) {
+        return "Hello, World!";
+    }
+
+}
